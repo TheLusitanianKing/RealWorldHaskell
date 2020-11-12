621f524c72d832c02cd3da75ebbded848737a36b
AlexLusitania <alex.lisboa.metalhead@gmail.com>
2020-11-12 20:06:59 +0000

    master : glob pattern into a regular expression

--- ch08/globRegex.hs
+++ ch08/globRegex.hs
@@ -8,6 +8,28 @@ import Text.Regex.Posix ((=~))
 globToRegex :: String -> String
 globToRegex cs = '^' : globToRegex' cs ++ "$"
 
-globToRegex' = undefined
+globToRegex' :: String -> String
+globToRegex' "" = ""
 
-matchesGlob = undefined
+globToRegex' ('*':cs) = ".*" ++ globToRegex' cs
+
+globToRegex' ('?':cs) = '.' : globToRegex' cs
+
+globToRegex' ('[':'!':c:cs) = "[^" ++ c : charClass cs
+globToRegex' ('[':c:cs)     = '['  :  c : charClass cs
+globToRegex' ('[':_)        = error "unterminated character class"
+
+globToRegex' (c:cs) = escape c ++ globToRegex' cs
+
+escape :: Char -> String
+escape c | c `elem` regexChars = '\\' : [c]
+         | otherwise = [c]
+    where regexChars = "\\+()^$.{}]|"
+
+charClass :: String -> String
+charClass (']':cs) = ']' : globToRegex' cs
+charClass (c:cs)   = c : charClass cs
+charClass []       = error "unterminated character class"
+
+matchesGlob :: FilePath -> String -> Bool
+name `matchesGlob` pat = name =~ globToRegex pat
