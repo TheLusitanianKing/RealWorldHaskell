dd3e517ab1d9273f71362ed1d56e2f92e2ced8fc
AlexLusitania <alex.lisboa.metalhead@gmail.com>
2020-11-18 20:43:18 +0000

    master : sizing a file safely

--- ch09/betterPredicate.hs
+++ ch09/betterPredicate.hs
@@ -2,7 +2,7 @@ import Control.Monad (filterM)
 import System.Directory (Permissions(..), getModificationTime, getPermissions)
 import Data.Time.Clock (UTCTime(..))
 import System.FilePath (takeExtension)
-import Control.Exception (bracket, handle)
+import Control.Exception (bracket, handle, SomeException(..))
 import System.IO (IOMode(..), hClose, hFileSize, openFile)
 
 import RecursiveContents (getRecursiveContents)
@@ -12,10 +12,6 @@ type Predicate =  FilePath      -- path to directory entry
                -> Maybe Integer -- file size (Nothing if not file)
                -> UTCTime       -- last modified
                -> Bool
-
--- soon to be defined
-getFileSize :: FilePath -> IO (Maybe Integer)
-getFileSize = undefined
 
 betterFind :: Predicate -> FilePath -> IO [FilePath]
 -- We can't use filter to call our predicate p,
@@ -28,3 +24,23 @@ betterFind p path = getRecursiveContents path >>= filterM check
             size <- getFileSize name
             modified <- getModificationTime name
             return (p name perms size modified)
+
+simpleFileSize :: FilePath -> IO Integer
+simpleFileSize path = do
+    h <- openFile path ReadMode
+    size <- hFileSize h
+    hClose h
+    return size
+
+saferFileSize :: FilePath -> IO (Maybe Integer)
+saferFileSize path = handle (\(SomeException _) -> return Nothing) $ do
+    h <- openFile path ReadMode
+    size <- hFileSize h
+    hClose h
+    return (Just size)
+
+getFileSize :: FilePath -> IO (Maybe Integer)
+getFileSize path = handle (\(SomeException _) -> return Nothing) $
+    bracket (openFile path ReadMode) hClose $ \h -> do
+        size <- hFileSize h
+        return (Just size)
