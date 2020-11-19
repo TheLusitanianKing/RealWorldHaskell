b80b62c5be6e6f461d6de1ac2c4b0d2b81b496ad
AlexLusitania <alex.lisboa.metalhead@gmail.com>
2020-11-19 14:25:24 +0000

    master : domain specific language for predicates

--- ch09/betterPredicate.hs
+++ ch09/betterPredicate.hs
@@ -14,7 +14,7 @@ type Predicate =  FilePath      -- path to directory entry
                -> Bool
 
 betterFind :: Predicate -> FilePath -> IO [FilePath]
--- We can't use filter to call our predicate p,
+-- we can't use filter to call our predicate p,
 -- as p's purity means it cannot do the I/O needed to gather the metadata it requires.
 -- filterM behaves like the normal filter function, but in this case it evaluates its predicate in the IO monad,
 -- allowing the predicate to perform I/O.
@@ -45,3 +45,29 @@ getFileSize path = handle (\(SomeException _) -> return Nothing) $
     bracket (openFile path ReadMode) hClose $ \h -> do
         size <- hFileSize h
         return (Just size)
+
+-- our predicate will check for a C++ source file that is over 128KB in size.
+myTest :: Predicate
+myTest path _ (Just size) _ =
+    takeExtension path == ".cpp" && size > 131072
+myTest _ _ _ _ = False
+
+type InfoP a =  FilePath      -- path to directory entry
+             -> Permissions   -- permissions
+             -> Maybe Integer -- file size (Nothing if not file)
+             -> UTCTime       -- last modified
+             -> a
+-- We could thus legitimately get rid of the Predicate type, as it is simply: InfoP Bool
+
+pathP :: InfoP FilePath
+pathP path _ _ _ = path
+
+sizeP :: InfoP Integer
+sizeP _ _ (Just size) _ = size
+sizeP _ _ Nothing _     = -1
+
+equalP :: (Eq a) => InfoP a -> a -> InfoP Bool
+equalP f k = \w x y z -> f w x y z == k
+
+equalP' :: (Eq a) => InfoP a -> a -> InfoP Bool
+equalP' f k w x y z = f w x y z == k
