118e75e00de3a8a420777ec43af85f912869124e
AlexLusitania <alex.lisboa.metalhead@gmail.com>
2020-11-21 17:44:01 +0000

    master : density, readability, and the learning process

--- ch09-io-case-study/controlledVisit.hs
+++ ch09-io-case-study/controlledVisit.hs
@@ -21,6 +21,22 @@ traverse order path = do
       if isDirectory info && infoPath info /= path
         then Main.traverse order (infoPath info)
         else return [info]
+        
+traverseVerbose :: ([Info] -> [Info]) -> FilePath -> IO [Info]
+traverseVerbose order path = do
+    names <- getDirectoryContents path
+    let usefulNames = filter (`notElem` [".", ".."]) names
+    contents <- mapM getEntryName ("" : usefulNames)
+    recursiveContents <- mapM recurse (order contents)
+    return (concat recursiveContents)
+  where getEntryName name = getInfo (path </> name)
+        isDirectory info = case infoPerms info of
+                             Nothing -> False
+                             Just perms -> searchable perms
+        recurse info = do
+            if isDirectory info && infoPath info /= path
+                then traverseVerbose order (infoPath info)
+                else return [info]
 
 
 getUsefulContents :: FilePath -> IO [String]
