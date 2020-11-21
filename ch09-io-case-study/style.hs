8ec51934ea7599a7d3e9249de938601b3cb15bc2
AlexLusitania <alex.lisboa.metalhead@gmail.com>
2020-11-21 23:55:56 +0000

    master : coding guidelines

---
+++ ch09-io-case-study/style.hs
@@ -0,0 +1,49 @@
+tidyLet = let foo = undefined
+              bar = foo * 2
+          in undefined
+
+weirdLet = let foo = undefined
+               bar = foo * 2
+    in undefined
+
+strangeLet = let foo = undefined
+                 bar = foo * 2 in
+    undefined
+
+-- commonDo = do
+--   something <- undefined
+--   return ()
+
+-- not seen very often
+-- rareDo =
+--   do something <- undefined
+--      return ()
+    
+
+unusualPunctuation =
+    [ (x,y) | x <- [1..a], y <- [1..b] ] where {
+                                           b = 7;
+ a = 6 }
+
+preferredLayout = [ (x,y) | x <- [1..a], y <- [1..b] ]
+    where b = 7
+          a = 6
+
+normalIndent =
+    undefined
+
+strangeIndent =
+                           undefined
+
+goodWhere = take 5 lambdas
+    where lambdas = []
+
+alsoGood =
+    take 5 lambdas
+  where
+    lambdas = []
+
+badWhere =           -- legal, but ugly and hard to read
+    take 5 lambdas
+    where
+    lambdas = []
