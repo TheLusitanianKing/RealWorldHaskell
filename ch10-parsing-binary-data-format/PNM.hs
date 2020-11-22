abe1f818b82936e75b3b9260142c0046ca3655b6
AlexLusitania <alex.lisboa.metalhead@gmail.com>
2020-11-22 19:03:52 +0000

    master : getting rid of boilerplate code

--- ch10-parsing-binary-data-format/PNM.hs
+++ ch10-parsing-binary-data-format/PNM.hs
@@ -15,6 +15,28 @@ data Greymap = Greymap {
 instance Show Greymap where
     show (Greymap w h m _) = "Greymap " ++ show w ++ "x" ++ show h ++ " " ++ show m
 
+matchHeader :: L.ByteString -> L.ByteString -> Maybe L.ByteString
+matchHeader prefix str
+    | prefix `L8.isPrefixOf` str
+        = Just (L8.dropWhile isSpace (L.drop (L.length prefix) str))
+    | otherwise
+        = Nothing
+
+-- "nat" here is short for "natural number"
+getNat :: L.ByteString -> Maybe (Int, L.ByteString)
+getNat s = case L8.readInt s of
+                Nothing -> Nothing
+                Just (x, rest)
+                    | x <= 0 -> Nothing
+                    | otherwise -> Just (fromIntegral x, rest)
+
+getBytes :: Int -> L.ByteString -> Maybe (L.ByteString, L.ByteString)
+getBytes n str = let count = fromIntegral n 
+                     both@(prefix, _) = L.splitAt count str
+                 in if L.length prefix < count
+                    then Nothing
+                    else Just both
+
 parseP5 :: L.ByteString -> Maybe (Greymap, L.ByteString)
 parseP5 s =
     case matchHeader (L8.pack "P5") s of
@@ -39,24 +61,22 @@ parseP5 s =
                                                     Just (bitmap, s6) ->
                                                         Just (Greymap width height maxGrey bitmap, s6)
 
-matchHeader :: L.ByteString -> L.ByteString -> Maybe L.ByteString
-matchHeader prefix str
-    | prefix `L8.isPrefixOf` str
-        = Just (L8.dropWhile isSpace (L.drop (L.length prefix) str))
-    | otherwise
-        = Nothing
+(>>?) :: Maybe a -> (a -> Maybe b) -> Maybe b
+Nothing >>? _ = Nothing
+Just v  >>? f = f v
 
--- "nat" here is short for "natural number"
-getNat :: L.ByteString -> Maybe (Int, L.ByteString)
-getNat s = case L8.readInt s of
-                Nothing -> Nothing
-                Just (x, rest)
-                    | x <= 0 -> Nothing
-                    | otherwise -> Just (fromIntegral x, rest)
+skipSpace :: (a, L.ByteString) -> Maybe (a, L.ByteString)
+skipSpace (a, s) = Just (a, L8.dropWhile isSpace s)
 
-getBytes :: Int -> L.ByteString -> Maybe (L.ByteString, L.ByteString)
-getBytes n str = let count = fromIntegral n 
-                     both@(prefix, _) = L.splitAt count str
-                 in if L.length prefix < count
-                    then Nothing
-                    else Just both
+parseP5_take2 :: L.ByteString -> Maybe (Greymap, L.ByteString)
+parseP5_take2 s =
+    matchHeader (L8.pack "P5") s      >>?
+    \s -> skipSpace ((), s)           >>?
+    (getNat . snd)                    >>?
+    skipSpace                         >>?
+    \(width, s) -> getNat s           >>?
+    skipSpace                         >>?
+    \(height, s) -> getNat s          >>?
+    \(maxGrey, s) -> getBytes 1 s     >>?
+    (getBytes (width * height) . snd) >>?
+    \(bitmap, s) -> Just (Greymap width height maxGrey bitmap, s)
