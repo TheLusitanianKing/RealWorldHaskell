af1ca5699ec370edffea20e56b5d70d98c4770e8
AlexLusitania <alex.lisboa.metalhead@gmail.com>
2020-12-02 19:00:52 +0000

    master : testing against a model

--- ch11-testing-quality-assurance/QC.hs
+++ ch11-testing-quality-assurance/QC.hs
@@ -30,3 +30,6 @@ prop_append xs ys =
     not (null xs) ==>
     not (null ys) ==>
         head (qsort (xs ++ ys)) == min (minimum xs) (minimum ys)
+
+-- testing against a model
+prop_sort_model xs = sort xs == qsort xs
