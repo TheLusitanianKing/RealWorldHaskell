ddc81a76ef80336b7bc12cd53d9a991104c8e09f
AlexLusitania <alex.lisboa.metalhead@gmail.com>
2020-12-02 18:59:23 +0000

    master : testing for properties

--- ch11-testing-quality-assurance/Main.hs
+++ ch11-testing-quality-assurance/Main.hs
@@ -6,4 +6,6 @@ import Test.QuickCheck
 main :: IO ()
 main = do
     quickCheck (prop_idempotent :: [Integer] -> Bool)
-    verboseCheck (prop_idempotent :: [Integer] -> Bool)
+    -- verboseCheck (prop_idempotent :: [Integer] -> Bool)
+    -- using the more general Property type here
+    quickCheck (prop_minimum :: [Integer] -> Property)
--- ch11-testing-quality-assurance/QC.hs
+++ ch11-testing-quality-assurance/QC.hs
@@ -1,7 +1,7 @@
 module QC where
 
 import Test.QuickCheck
-import Data.List
+import Data.List ((\\))
 
 qsort :: Ord a => [a] -> [a]
 qsort []     = []
@@ -10,3 +10,23 @@ qsort (x:xs) = qsort lhs ++ [x] ++ qsort rhs
           rhs = filter (>= x) xs
 
 prop_idempotent xs = qsort (qsort xs) == qsort xs
+
+-- as head and minimum do not work on empty lists
+-- using the (==>) implication function, we filter out invalid data before running the property
+prop_minimum xs = not (null xs) ==> head (qsort xs) == minimum xs
+
+prop_ordered xs = ordered (qsort xs)
+    where ordered []       = True
+          ordered [x]      = True
+          ordered (x:y:xs) = x <= y && ordered (y:xs)
+
+prop_permutation xs = permutation xs (qsort xs)
+    where permutation xs ys = null (xs \\ ys) && null (ys \\ xs)
+    -- (\\) is the list difference function
+
+prop_maximum xs = not (null xs) ==> last (qsort xs) == maximum xs
+
+prop_append xs ys =
+    not (null xs) ==>
+    not (null ys) ==>
+        head (qsort (xs ++ ys)) == min (minimum xs) (minimum ys)
