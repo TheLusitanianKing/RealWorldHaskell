1a5aca4639d84d39c29f02c2e62fc714b947b91b
TheLusitanianKing <alex.lisboa.metalhead@gmail.com>
2021-01-21 22:14:30 +0000

    a bit more state?

--- ch14-monads/Random.hs
+++ ch14-monads/Random.hs
@@ -41,3 +41,29 @@ runTwoRandoms = do
     let (result, newState) = runState getTwoRandoms oldState
     setStdGen newState
     return result
+
+data CountedRandom = CountedRandom {
+        crGen :: StdGen,
+        crCount :: Int
+    }
+type CRState = State CountedRandom
+
+getCountedRandom :: Random a => CRState a
+getCountedRandom = do
+    st <- get
+    let (val, gen') = random (crGen st)
+    put CountedRandom { crGen = gen', crCount = crCount st + 1 }
+    return val
+
+getCount :: CRState Int
+getCount = crCount `liftM` get
+-- as monads and functors are closely related
+-- fmap lifts a pure function into the monad, just as liftM does
+
+putCount :: Int -> CRState ()
+putCount a = do
+    st <- get
+    put st { crCount = a }
+
+putCountModify :: Int -> CRState ()
+putCountModify a = modify $ \st -> st { crCount = a }
